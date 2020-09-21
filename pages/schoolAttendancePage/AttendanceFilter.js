22727361192363074da8b9d972dad661751cbb01
pengchihui1 <pengchihui1@gmail.com>
2020-09-21 18:30:04 +0800

    考勤首頁完成，沒做兼容

--- pages/schoolAttendancePage/AttendanceFilter.js
+++ pages/schoolAttendancePage/AttendanceFilter.js
@@ -12,7 +12,12 @@ import Container from '../../components/container'
 
 const AttendanceFilter = ({ ...props }) => {
   const [value, setValue] = React.useState('1')
-
+  const Attendance = [
+    { name: '未到/準時', color: '#FFFFFF' },
+    { name: ' 遲到', color: '#FFFF00' },
+    { name: '  缺席', color: '#1C86EE' },
+    { name: ' 曠課', color: '#CD6090' }
+  ]
   return (
     <Container>
       {/* 搜索 */}
@@ -23,7 +28,7 @@ const AttendanceFilter = ({ ...props }) => {
         <IconButton aria-label='Search database' variantColor='blue' icon='search' ml={4} />
       </Box>
       {/* 考勤篩子按鈕 全部 未到/準時 遲到 缺席 曠課    */}
-      <RadioGroup mt='15px' textAlign='center' defaultValue='2' spacing={5} isInline onChange={e => setValue(e.target.value)} value={value}>
+      <RadioGroup mt={5} textAlign='center' defaultValue='2' spacing={5} isInline onChange={e => setValue(e.target.value)} value={value}>
         <Radio value='1'>
           全部
         </Radio>
@@ -41,12 +46,19 @@ const AttendanceFilter = ({ ...props }) => {
         </Radio>
       </RadioGroup>
       {/* select 上午 下午 全部 未到/準時 遲到 缺席 曠課 不同的顔色   */}
-      <Box mt='15px' m='0 auto'>
-        <Select maxW='80px'>
+      <Box d='flex' mt={2} justifyContent='center'>
+        <Select w='86px' h='30px'>
           <option value='option1'>上午</option>
           <option value='option2'>下午</option>
         </Select>
-        <Box>1545</Box>
+        {Attendance.map((item, i) => {
+          return (
+            <Box d='flex' justifyContent='center' alignItems='center' ml={6} key={i}>
+              <Box bg={item.color} w='24px' h='24px' border='1px solid #ddd' />
+              <Box ml='5px'>{item.name}</Box>
+            </Box>
+          )
+        })}
       </Box>
     </Container>
   )
--- pages/schoolAttendancePage/AttendanceTable.js
+++ pages/schoolAttendancePage/AttendanceTable.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
-
+  Box,
+  Button,
   Table,
   TableHead,
   TableCell,
