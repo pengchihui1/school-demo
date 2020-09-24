4d5ebfd2070845109b5e60bd65312a793721a375
pengchihui1 <pengchihui1@gmail.com>
2020-09-24 16:43:53 +0800

    搬運

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-// import 'react-dates/lib/css/_datepicker.css'
+// import 'react-dates/lib/css/_datepicker.css'  多出一行
 import 'react-datepicker/dist/react-datepicker.css'
 
-export default function MyApp ({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
