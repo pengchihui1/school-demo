e7fbc8009072dd5964c4a4b81b8217ed9cb2d730
pengchihui1 <pengchihui1@gmail.com>
2020-11-21 13:14:17 +0800

    版本不兼容

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,36 @@
+// import React from 'react'
 import { Box } from 'viviui'
 import App from '../App'
-import { launch, renderWhenReady } from '@extjs/reactor'
-import { Panel, Grid, Column } from '@extjs/reactor/classic'
+// import { launch } from '@extjs/reactor'
+// import { Panel, Grid, Column } from '@extjs/reactor/classic'
+
+// install({ viewport: true })
+
+// Ext.onReady{
+//   () => {
+//     ReactDOM.render(document.getElementById('root'))
+//   }
+// }
+// Ext.require('Ext.window.Toast')
+
+// Ext.namespace('Ext.ux');
 
 const Index = () => {
-  const store = Ext.create('Ext.data.Store', {
-    fields: ['name', 'email'],
-    data: [
-      { name: 'Tim Smith', email: 'tim101@gmail.com' },
-      { name: 'Jill Lindsey', email: 'jlindsey890@gmail.com' }
-    ]
-  })
+  // const store = install.create('Ext.data.Store', {
+  //   fields: ['name', 'email'],
+  //   data: [
+  //     { name: 'Tim Smith', email: 'tim101@gmail.com' },
+  //     { name: 'Jill Lindsey', email: 'jlindsey890@gmail.com' }
+  //   ]
+  // })
   return (
     <App>
-      <Grid title='Users' store={store}>
+      {/* <Grid title='Users' store={store}>
         <Column text='Name' dataIndex='name' />
         <Column text='Email' dataIndex='email' />
-      </Grid>
+      </Grid> */}
+      <Box> "react-dom":"^15.4.0"</Box>
     </App>
   )
 }
-export default launch(Index)
+export default Index
