0e33d9c2e8bb3757c260ea8bee6b7a6521690856
pengchihui1 <pengchihui1@gmail.com>
2020-11-23 14:20:37 +0800

    123

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
-import { Box } from 'viviui'
+import { Box, Button, useToast } from 'viviui'
 export default () => {
+  const toast = useToast()
   return (
-    <Box>123456</Box>
+    <>
+      <Box>~cypress.io</Box>
+      <Button
+        onClick={() =>
+          toast({
+            title: 'Account created.',
+            description: "We've created your account for you.",
+            status: 'success',
+            duration: 9000,
+            isClosable: true
+          })}
+      >
+        Show Toast
+      </Button>
+    </>
   )
 }
