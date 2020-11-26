ea9e4f83ce999633e112f78b2d537a591a671b3c
pengchihui1 <pengchihui1@gmail.com>
2020-11-26 12:47:30 +0800

    配置全局样式

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,42 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react'
 import { Box, Button } from 'viviui'
 import NextLink from 'next/link'
 import App from '../App'
 
-import _ from "lodash";
-import ReactEcharts from 'echarts-for-react';
-import { getBarChart, getLineChart, getPieChart } from "./chart";
-import { Layout, Button } from 'antd';
-import { WidthProvider, Responsive } from "react-grid-layout";
+// import ReactEcharts from 'echarts-for-react'
+// import { getBarChart, getLineChart, getPieChart } from './chart'
+// import { Layout } from 'antd'
+// import { WidthProvider, Responsive } from 'react-grid-layout'
+import { AgGridColumn, AgGridReact } from 'ag-grid-react'
 
-import * as serviceWorker from './serviceWorker'
+import _ from 'lodash'
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
-const { Header, Content } = Layout;
+const Home = () => {
+  const [gridApi, setGridApi] = useState(null)
+  const [gridColumnApi, setGridColumnApi] = useState(null)
+  const [rowData, setRowData] = useState([
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ])
+  function onGridReady(params) {
+    setGridApi(params.api)
+    setGridColumnApi(params.columnApi)
+  }
 
-serviceWorker.unregister()
-
-const Home = ({ props }) => {
-  const ResponsiveReactGridLayout = WidthProvider(Responsive)
   return (
     <App>
       <NextLink href='https://juejin.cn/post/6844903843436691463'><Button>拖拽布局</Button></NextLink>
-      <ResponsiveReactGridLayout
-        className='layout'
-        // {props}
-        layouts={layouts}
-        onLayoutChange={(layout, layouts) =>
-          this.onLayoutChange(layout, layouts)}
-      >
-        {this.generateDOM()}
-      </ResponsiveReactGridLayout>
+      <Box className='ag-theme-alpine' style={{ height: 400, width: 600 }}>
+        <AgGridReact
+          onGridReady={onGridReady}
+          rowData={rowData}
+        >
+          <AgGridColumn field='make' />
+          <AgGridColumn field='model' />
+          <AgGridColumn field='price' />
+        </AgGridReact>
+      </Box>
     </App>
   )
 }
