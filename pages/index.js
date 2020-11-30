6d79d33e0eaf4d388faeaed574fe6c5079be543b
pengchihui1 <pengchihui1@gmail.com>
2020-11-30 11:39:21 +0800

    可以渲染不能拖动

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,111 @@
 import { Box } from 'viviui'
 import App from '../App'
-import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
-const Home = () => {
-  const arr = {
-    short_name: 'React App',
-    name: 'Create React App Sample',
-    icons: [
-      {
-        src: 'favicon.ico',
-        sizes: '64x64 32x32 24x24 16x16',
-        type: 'image/x-icon'
-      }
-    ],
-    start_url: '.',
-    display: 'standalone',
-    theme_color: '#000000',
-    background_color: '#ffffff'
+// fake data generator
+const getItems = (count) => Array.from({ length: count }, (v, k) => k).map(k => ({
+  id: `item-${k}`,
+  content: `item ${k}`
+}))
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
+
+// using some little inline style helpers to make the app look okay
+const grid = 8
+const getItemStyle = (draggableStyle, isDragging) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  marginBottom: grid,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+})
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250
+})
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: getItems(10)
+    }
+    this.onDragEnd = this.onDragEnd.bind(this)
   }
 
-  return (
-    <App id='root'>
-      <Box>45689</Box>
-    </App>
-  )
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    )
+
+    this.setState({
+      items
+    })
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId='droppable'>
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {this.state.items.map(item => (
+                <Draggable
+                  key={item.id}
+                  draggableId={item.id}
+                >
+                  {(provided, snapshot) => (
+                    <div>
+                      <div
+                        ref={provided.innerRef}
+                        style={getItemStyle(
+                          provided.draggableStyle,
+                          snapshot.isDragging
+                        )}
+                        {...provided.dragHandleProps}
+                      >
+                        {item.content}
+                      </div>
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    )
+  }
 }
 
 export default Home
+// Put the thing into the DOM!
+// ReactDOM.render(<Home />, document.getElementById('app'))
