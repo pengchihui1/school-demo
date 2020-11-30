e3532df01555879978d5d83da4b45a1bfc08e4ce
pengchihui1 <pengchihui1@gmail.com>
2020-11-30 11:20:25 +0800

    安装拖住插件

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import { Box } from 'viviui'
 import App from '../App'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
-export default () => {
+const Home = () => {
+  const arr = {
+    short_name: 'React App',
+    name: 'Create React App Sample',
+    icons: [
+      {
+        src: 'favicon.ico',
+        sizes: '64x64 32x32 24x24 16x16',
+        type: 'image/x-icon'
+      }
+    ],
+    start_url: '.',
+    display: 'standalone',
+    theme_color: '#000000',
+    background_color: '#ffffff'
+  }
+
   return (
-    <App>
-      <Box>123456</Box>
+    <App id='root'>
+      <Box>45689</Box>
     </App>
   )
 }
+
+export default Home
